17768ee497bc53343728ffa599a18d6b3fbb3ff0
LukElvetico <Weber.ltm@gmail.com>
2025-10-28 21:25:27 +0100

    check errori

--- Frontend/ecommerce/src/components/HomePage.jsx
+++ Frontend/ecommerce/src/components/HomePage.jsx
@@ -344,7 +344,7 @@ const HomePage = () => {
                 <h1 className="display-4 fw-bolder text-primary">Welcome in EpiCommerce</h1>
                 <p className="lead mt-3 text-muted">Crea lo smartphone dei tuoi sogni e scopri cosa dicono gli altri clienti!</p>
             </header>
-            <SuggestedCarouselSelection/>
+            <SuggestedProductsSection/>
             <ProductCarouselSection />
             <RandomPostSection />
             <CallToActionSection />
